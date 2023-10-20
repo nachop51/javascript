b51302795b72a788aa92b51560e937622b196e9f
Nachop51 <nachoperalta0@gmail.com>
2023-10-20 00:33:20 -0300

    MVC applied

---
+++ mvc_express/models/movie.js
@@ -0,0 +1,57 @@
+import { randomUUID } from 'node:crypto'
+import { readJSON } from '../utils.js'
+
+const movies = readJSON('./movies.json')
+
+export class MovieModel {
+  static async getAll ({ genre }) {
+    if (genre) {
+      return movies.filter(movie =>
+        movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+      )
+    }
+
+    return movies
+  }
+
+  static async getById ({ id }) {
+    const movie = movies.find((movie) => movie.id === id)
+    return movie
+  }
+
+  static async create (input) {
+    const newMovie = {
+      id: randomUUID(),
+      ...input
+    }
+
+    movies.push(newMovie)
+
+    return newMovie
+  }
+
+  static async delete ({ id }) {
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) return null
+
+    movies.splice(movieIndex, 1)
+
+    return true
+  }
+
+  static async update ({ id, input }) {
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) return null
+
+    const updatedMovie = {
+      ...movies[movieIndex],
+      ...input
+    }
+
+    movies[movieIndex] = updatedMovie
+
+    return updatedMovie
+  }
+}
--- mvc_express/routes/movies.js
+++ mvc_express/routes/movies.js
@@ -1,82 +1,11 @@
 import { Router } from 'express'
-import { readJSON } from '../utils.js'
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-import randomUUID from 'node:crypto'
-
-const movies = readJSON('./movies.json')
+import { MovieController } from '../controllers/movies.js'
 
 export const moviesRouter = Router()
 
-moviesRouter.get('/', (req, res) => {
-  const genre = req.query.genre
+moviesRouter.get('/', MovieController.getAll)
+moviesRouter.post('/', MovieController.create)
 
-  if (genre) {
-    const filteredMovies = movies.filter(movie =>
-      movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-    )
-
-    return res.json(filteredMovies)
-  }
-
-  res.json(movies)
-})
-
-moviesRouter.get('/:id', (req, res) => {
-  const { id } = req.params
-  const movie = movies.find((movie) => movie.id === id)
-  if (movie) return res.json(movie)
-
-  res.status(404).json({ message: 'Movie not found' })
-})
-
-moviesRouter.post('/', (req, res) => {
-  const result = validateMovie(req.body)
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const newMovie = {
-    id: randomUUID(),
-    ...result.data
-  }
-
-  movies.push(newMovie)
-
-  res.status(201).json(newMovie)
-})
-
-moviesRouter.delete('/:id', (req, res) => {
-  const { id } = req.params
-
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) return res.status(404).json({ message: 'Movie not found' })
-
-  movies.splice(movieIndex, 1)
-
-  res.status(204).json({ message: 'Movie deleted' })
-})
-
-moviesRouter.patch('/:id', (req, res) => {
-  const { id } = req.params
-
-  const movieIndex = movies.findIndex(movie => movie.id === id)
-
-  if (movieIndex === -1) return res.status(404).json({ message: 'Movie not found' })
-
-  const result = validatePartialMovie(req.body)
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const updatedMovie = {
-    ...movies[movieIndex],
-    ...result.data
-  }
-
-  movies[movieIndex] = updatedMovie
-
-  res.json(updatedMovie)
-})
+moviesRouter.get('/:id', MovieController.getById)
+moviesRouter.delete('/:id', MovieController.delete)
+moviesRouter.patch('/:id', MovieController.update)
