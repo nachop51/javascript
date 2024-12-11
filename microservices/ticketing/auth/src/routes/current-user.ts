f44a6a328097646508933e379b5403d6fd3e98ed
nachop51 <nachoperalta0@gmail.com>
2024-12-10 23:24:40 -0300

    auth logic

--- microservices/ticketing/auth/src/routes/current-user.ts
+++ microservices/ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!')
+  if (!req.session?.jwt) {
+    res.send({ currentUser: null })
+    return
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!)
+    res.send({ currentUser: payload })
+  } catch (err) {
+    res.send({ currentUser: null })
+  }
 })
 
 export { router as currentUserRouter }
--- microservices/ticketing/auth/src/routes/signin.ts
+++ microservices/ticketing/auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { type Request, type Response } from 'express'
 import { body } from 'express-validator'
 import { validateRequest } from '../middlewares/validate-request'
+import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error'
+import { Password } from '../services/password'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
@@ -11,8 +15,35 @@ router.post(
     body('password').trim().notEmpty().withMessage('You must supply a password')
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.send({})
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials')
+    }
+
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    )
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid credentials')
+    }
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser._id,
+        email: existingUser.email
+      },
+      process.env.JWT_KEY!
+    )
+
+    req.session = {
+      jwt: userJwt
+    }
   }
 )
 
